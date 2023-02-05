b0eb8d5c9fcf4ba225770393f9c459ec1ae53dea
Mark Harvey <mark990521@gmail.com>
2023-02-05 19:28:49 +0100

    .

--- deploy.sh
+++ deploy.sh
@@ -3,28 +3,22 @@
 # abort on errors
 set -e
 
-
 # build
 npm run build
-
-cd dist
 
 touch .nojekyll
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git checkout -B dist
-git add -f .
+# git checkout --orphan dist
+git add -f dist/ .nojekyll
 git commit -m 'deploy'
-git push -f origin dist
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com/MarkXVI/MarkXVI.github.io.git dist
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 git checkout main
-
-cd ..
